84913e53188876e87b976696c87879025f26c340
deepak dhaked <deepakdhakedpc@gmail.com>
2023-06-29 04:15:32 +0530

    another method is added

--- happy_number.cpp
+++ happy_number.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include <set>
 using namespace std;
+
+int numSquareSum(int n)
+{
+    int squareSum = 0;
+    while (n)
+    {
+        squareSum += (n % 10) * (n % 10);
+        n /= 10;
+    }
+    return squareSum;
+}
 
 // Time Complexity: O(n*log(n)).
 // Auxiliary Space: O(1)
@@ -33,10 +45,26 @@ bool isHappy(int N)
     return false;
 }
 
+// Time Complexity: O(n*log(n)).
+// Auxiliary Space: O(n) since using extra set for storage
+bool isHappy2(int N)
+{
+    set<int> s;
+    while (1)
+    {
+        N = numSquareSum(N);
+        if (N == 1)
+            return true;
+        if (s.find(N) != s.end())
+            return false;
+        s.insert(N);
+    }
+}
+
 int main()
 {
-    int n = 12;
-    if (isHappy(n) == true)
+    int n = 7;
+    if (isHappy2(n) == true)
     {
         cout << "Happy Number" << endl;
     }

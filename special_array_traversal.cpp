d014d1a51923e94abc4bf9ff6fe588d56a8a8893
deepak dhaked <deepakdhakedpc@gmail.com>
2023-07-09 23:32:13 +0530

    special array traversal is added

---
+++ special_array_traversal.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+string reverse(string s)
+{
+    stack<char> char_stack;
+    for (char ch : s)
+    {
+        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+        {
+            char_stack.push(ch);
+        }
+    }
+    string ans = "";
+    for (char ch : s)
+    {
+        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+        {
+            ans += char_stack.top();
+            char_stack.pop();
+        }
+        else
+        {
+            ans += ch;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    string s =~ "A&x#";
+    cout << reverse(s) << endl;
+}

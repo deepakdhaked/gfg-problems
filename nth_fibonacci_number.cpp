d4cc7fb9a4a8107f35e0de8a4460385cd267fbfb
deepak dhaked <deepakdhakedpc@gmail.com>
2023-08-13 06:39:50 +0530

    bottom up approach is added with space optimization

--- nth_fibonacci_number.cpp
+++ nth_fibonacci_number.cpp
@@ -17,8 +17,36 @@ int func(int n, vector<int> &dp)
     return dp[n];
 }
 
+// recrsive approach (top-down approach)
+// Time Complexity : O(N)
+// Space Complexity : O(N)
 int nthFibonacci(int n)
 {
     vector<int> dp(n + 1, -1);
     return func(n, dp) % mod;
+}
+
+// iterative approach (bottom-up approach)
+// Time Complexity : O(N)
+// Space Complexity : O(1)
+int nthFibonacci(int n)
+{
+    int second_previous = 0;
+    int first_previous = 1;
+
+    if (n == 0)
+    {
+        return second_previous;
+    }
+
+    int current;
+    for (int i = 2; i <= n; i++)
+    {
+        current = (first_previous + second_previous) % mod;
+
+        // update second_previous and first_previous
+        second_previous = first_previous;
+        first_previous = current;
+    }
+    return first_previous;
 }
